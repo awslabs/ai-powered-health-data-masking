661324946652307a4730ec39b91d498abc4827e8
ssenior <ssenior@amazon.com>
2019-09-05 09:29:35 -0400

    Update to template desc
    Update to template desc

--- source/get_text_phi/test_get_text_phi.py
+++ source/get_text_phi/test_get_text_phi.py
@@ -6,19 +6,19 @@ class TestRequestHeaders(unittest.TestCase):
 
     def test_validate_parameters(self):
         # One correct param
-        event_correct_1 = {"body": "{\"text\": \"Aaron Friedman was born on April 1 0, 1987\"}"}
+        event_correct_1 = {"body": "{\"text\": \"Mary Major was born on April 1 0, 1987\"}"}
         # One incorrect param
-        event_incorrect_1param = {"body": "{\"phiDectionThreshold\": \"Aaron Friedman was born on April 1 6, 1987\"}"}
+        event_incorrect_1param = {"body": "{\"phiDectionThreshold\": \"Mary Major was born on April 1 6, 1987\"}"}
         # Two correct params
-        event_correct_2 = {"body": "{\"text\": \"Aaron Friedman was born on April 1 2, 1987\", \"phiDetectionThreshold\":0.5}"}
+        event_correct_2 = {"body": "{\"text\": \"Mary Major was born on April 1 2, 1987\", \"phiDetectionThreshold\":0.5}"}
         # Two correct params, incorrect threshold
-        event_incorrect_threshold = {"body": "{\"text\": \"Aaron Friedman was born on April 1 1, 1987\", \"phiDetectionThreshold\":1.5}"}
+        event_incorrect_threshold = {"body": "{\"text\": \"Mary Major was born on April 1 1, 1987\", \"phiDetectionThreshold\":1.5}"}
         # Two params, one correct
-        event_incorrect_param2 = {"body": "{\"text\": \"Aaron Friedman was born on April 1 3, 1987\", \"phectionThreshold\":0.5}"}
+        event_incorrect_param2 = {"body": "{\"text\": \"Mary Major was born on April 1 3, 1987\", \"phectionThreshold\":0.5}"}
         # Body missing
-        event_body_missing = {"bodysadfas": "{\"text\": \"Aaron Friedman was born on April 1 4, 1987\", \"phiDetectionThreshold\":0.5}"}
+        event_body_missing = {"bodysadfas": "{\"text\": \"Mary Major was born on April 1 4, 1987\", \"phiDetectionThreshold\":0.5}"}
         # Two params, same field
-        event_duplicate_field = {"body": "{\"text\": \"Aaron Friedman was born on April 1 3, 1987\", \"text\":0.5}"}
+        event_duplicate_field = {"body": "{\"text\": \"Mary Major was born on April 1 3, 1987\", \"text\":0.5}"}
         
         self.assertTrue(validate_parameters(event_correct_1))
         self.assertFalse(validate_parameters(event_incorrect_1param))
